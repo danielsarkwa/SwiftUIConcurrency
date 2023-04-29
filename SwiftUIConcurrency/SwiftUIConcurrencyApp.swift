8960a5efdf921cfa1e75c561b1f0eaf5c851174d
DanielSarkwa <dhannysark@outlook.com>
2023-04-29 13:17:53 +0300

    Initial Commit

---
+++ SwiftUIConcurrency/SwiftUIConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIConcurrencyApp.swift
+//  SwiftUIConcurrency
+//
+//  Created by Daniel Yeboah on 29.4.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
